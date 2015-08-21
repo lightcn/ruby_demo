22b8ed9e45c61d3a366c58f479e362a852a4668f
张 志亮 <lightcn@hotmail.com>
2015-08-21 10:58:12 +0800

    new

---
+++ hashx.rb
@@ -0,0 +1,9 @@
+h = {
+  :one => 1,
+  :two => 2
+}
+h[:one]
+h[:three] = 3
+h.each do |key, value|
+  print "#{value}:#{key}; "
+end
---
+++ times20.rb
@@ -0,0 +1,2 @@
+3.times { print "Ruby! " }
+1.upto(9) { |x| print x}
